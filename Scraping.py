e28e43a40c22e885d0f16350bbb44b1adc82f007
V-Carvalho <vinicius.a.carvalho@outlook.com>
2022-08-27 18:59:26 -0300

    Adicionado o arquivo: requirements

--- Scraping.py
+++ Scraping.py
@@ -2,7 +2,7 @@ import os
 import time
 import dotenv
 import asyncio
-from Processing import Processing
+from processing import Processing
 from playwright.async_api import async_playwright
 
 processing = Processing()
